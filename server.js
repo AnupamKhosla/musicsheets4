03ab82d1cea16d4557abc96bae4d069ff79522cc
Anon <anupamkhosla1993@gmail.com>
2023-07-09 20:19:23 +0530

    added mongo database

--- server.js
+++ server.js
@@ -6,24 +6,63 @@
 import express from 'express';
 import path from 'node:path';
 
-//console.log(path);
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import "express-async-errors";
+import db from "./db/conn.mjs";
 
 
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 var app = express(),
-//path = require('path'),
 
 // __dirname is path.resolve()
-
 //publicDir = path.join(__dirname,'public');
 publicDir = path.join(path.resolve(),'public');
 
 
-app.use(express.static(publicDir))
+app.use(cors());
+app.use(express.static(publicDir));
 
-app.listen(port);
+app.use("/latest", async (req, res) => {
+  let collection = await db.collection("posts");
+  let results = await collection.aggregate([
+    {"$project": {"author": 1, "title": 1, "tags": 1, "date": 1}},
+    {"$sort": {"date": -1}},
+    {"$limit": 3}
+  ]).toArray();
+  res.send(results).status(200);
+});
 
+
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
+// app.listen(port);
+// console.log("listen started");
 
 //module.exports = app;
 export default app;
+
+///////////////////
+
+// app.use(express.json());
+
+// // Load the /posts routes
+// app.use("/posts", posts);
+
+// // Global error handling
+// app.use((err, _req, res, next) => {
+//   res.status(500).send("Uh oh! An unexpected error occured.")
+// })
+
+// // start the Express server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
